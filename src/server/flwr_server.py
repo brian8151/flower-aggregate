1fbe22bad96052f089d98939ad872e34ba762052
brian8151 <xunwu8151@yahoo.com>
2024-04-30 16:20:00 -0400

    add logs

--- src/server/flwr_server.py
+++ src/server/flwr_server.py
@@ -4,15 +4,29 @@ from flwr.server import ServerApp, ServerConfig
 from flwr.server.strategy import FedAvg
 from flwr.common import Metrics
 
+from src.util import log
+logger = log.init_logger()
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
-    # Multiply accuracy of each client by number of examples used
+    # Calculate weighted accuracies
     accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
     examples = [num_examples for num_examples, _ in metrics]
 
+    # Log each client's accuracy and the number of examples used
+    for i, (acc, ex) in enumerate(zip(accuracies, examples)):
+        logger.info(f"Client {i}: Accuracy={acc / ex}, Examples={ex}")
+
+    total_examples = sum(examples)
+    total_weighted_accuracy = sum(accuracies)
+    weighted_avg_accuracy = total_weighted_accuracy / total_examples
+    # Log aggregate information
+    logger.info(f"Total Examples: {total_examples}")
+    logger.info(f"Total Weighted Accuracy: {total_weighted_accuracy}")
+    logger.info(f"Weighted Average Accuracy: {weighted_avg_accuracy}")
+
     # Aggregate and return custom metric (weighted average)
-    return {"accuracy": sum(accuracies) / sum(examples)}
+    return {"accuracy": weighted_avg_accuracy}
 
 
 # Define strategy
