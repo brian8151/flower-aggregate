6450126d3c63401132b0314b760169a0ce66e266
brian8151 <xunwu8151@yahoo.com>
2024-04-29 20:30:43 -0400

    agg scripts

--- script/run-agg.sh
+++ script/run-agg.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
 PATH=/home/ec2-user/flwr-test/flower-aggregate
-
+LOG_PATH=/home/ec2-user/flwr-test/
 cd $PATH
+
 
 # Print the starting directory
 echo "Running from flower-aggregate directory: $(pwd)"
-
 # Default to 'start' if no argument is given
 cmd=${1:-start}
 
+# Define the function to setup the virtual environment and install dependencies
+setup_env() {
+    if [ ! -d "venv" ]; then
+        echo "Virtual environment not found. Creating one..."
+        python3 -m venv venv
+        echo "Virtual environment created."
+
+        echo "Activating virtual environment..."
+        source venv/bin/activate
+
+        echo "Installing dependencies from requirements.txt..."
+        pip install -r requirements.txt
+
+        echo "Dependencies installed."
+    else
+        echo "Virtual environment already exists."
+        echo "Activating virtual environment..."
+        source venv/bin/activate
+    fi
+}
+
+
 # Define the function to start the application
 start_app() {
-    echo "Activating virtual environment..."
-    source ./venv/bin/activate
-    echo "Starting Python flower agg application in the background..."
-    nohup python -m flower-aggregate > flwr-agg.log 2>&1 &
-    echo $! > flower-agg.pid
-    echo "Flower agg started with PID $(cat flower-agg.pid)"
+    setup_env
+    echo "Starting Flower agg application..."
+    nohup python -m flower-aggregate > $LOG_PATH/flwr-agg.log 2>&1 &
+    echo "Flower agg started in the background, logs: $LOG_PATH/flwr-agg.log"
 }
 
 # Define the function to stop the application
 stop_app() {
-    echo "Stopping flower agg..."
-    if [ -f flower.pid ]; then
-        kill $(cat flower-agg.pid) && rm flower-agg.pid
+    pid=$(ps aux | grep -i 'flower-aggregate' | grep -v grep | awk '{print $2}')
+    if [ ! -z "$pid" ]; then
+        echo "Found process $pid, shutting down flower-aggregate..."
+        kill -9 $pid
         echo "Flower agg stopped."
     else
-        echo "No PID file found. Is the application running?"
+        echo "No process found for flower-aggregate."
     fi
     echo "Deactivating virtual environment..."
     deactivate
