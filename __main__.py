b5ddeb8dcd09ec0109dfe054a38196aa23885324
brian8151 <xunwu8151@yahoo.com>
2024-05-08 13:51:08 -0400

    update main

--- __main__.py
+++ __main__.py
@@ -2,9 +2,13 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from src.router.flower_router import flower_router
-
+import logging
 def create_app():
     app = FastAPI()
+
+    @app.on_event("startup")
+    def on_startup():
+        logging.info("Server started and is listening on http://0.0.0.0:8000")
     # Set up CORS
     app.add_middleware(
         CORSMiddleware,
@@ -19,5 +23,6 @@ def create_app():
 
 # Legacy mode
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     app = create_app()
     uvicorn.run(app, host="0.0.0.0", port=8000)
