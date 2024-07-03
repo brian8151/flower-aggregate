fd2754f1505441f579ced5d4b490fd7d67d30f6c
brian8151 <xunwu8151@yahoo.com>
2024-07-03 12:11:47 -0400

    update model id

--- src/model/aggregate_request.py
+++ src/model/aggregate_request.py
@@ -5,6 +5,6 @@ from typing import List, Optional
 class AggregatorRequest(BaseModel):
     domain_type: str = Field(..., alias="domainType", description="data seed domain type")
     workflow_trace_id: str = Field(..., alias="workflowTraceId", description="workflow trace id")
-    model_id: str = Field(..., alias="modelId", description="model id")
+    model_id: int = Field(..., alias="modelId", description="model id")
     group_hash: str = Field(..., alias="groupHash", description="group Hash")
 
