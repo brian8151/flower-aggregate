5297dbb55d3ad50c0545a99a5094b481c9bd9b15
brian8151 <xunwu8151@yahoo.com>
2024-07-03 06:08:13 -0400

    add missing import

--- src/service/aggregator_runner_service.py
+++ src/service/aggregator_runner_service.py
@@ -1,6 +1,6 @@
 import numpy as np
 from src.util import log
-from src.repository.model.model_data_repositoty import get_model_client_training_record, save_model_aggregate_result
+from src.repository.model.model_data_repositoty import get_model_client_training_record, save_model_aggregate_result, get_model_info
 from src.ml.model_builder import decompress_weights, compress_weights
 from flwr.server.strategy.fedavg import FedAvg
 from flwr.server.client_proxy import ClientProxy
